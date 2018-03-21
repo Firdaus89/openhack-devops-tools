47c6620b59237597ee8accd129bdaf2b06cd3ee7
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-03-21 15:58:28 +0900

    Share this for the review. orchestrator.go hasn't tested yet.

--- leaderboard/sentinel/orchestrator.go
+++ leaderboard/sentinel/orchestrator.go
@@ -1,9 +1,61 @@
 package main
 
-// Using a Channel to cancel this request.
+import (
+	"sync"
+)
+
+// RunAllPokers pokes all services and update the status
 func RunAllPokers() {
 	// Read all Teams
-	// Parallel execution of
+	c := GetTeamsChanel(ReadAllTeams())
+	// Parallel execution of Check Status per each Team
 	// Wait for all finished
-	// Show the messages
+	newTeamsChanel := ExecAsync(c)
+	// Update the status
+	UpdateTeamStatus(newTeamsChanel)
+}
+
+func GetTeamsChanel(teams *[]Team) <-chan Team {
+	out := make(chan Team)
+	go func() {
+		for _, n := range *teams {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func ExecAsync(teams ...<-chan Team) <-chan Team {
+	var wg sync.WaitGroup
+	out := make(chan Team)
+	output := func(ts <-chan Team) {
+		for t := range ts {
+			t.StatusCheck()
+			out <- t
+		}
+		wg.Done()
+	}
+	wg.Add(len(teams))
+	for _, t := range teams {
+		go output(t)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
+
+// ReadAllTeams reads from MongoDB the Team, Challenge, Service, History
+func ReadAllTeams() *[]Team {
+	// Read Teams from MongoDB
+	return nil
+}
+
+// UpdateTeamStatus updates MongoDB's team Status
+func UpdateTeamStatus(teams ...<-chan Team) error {
+	// Write Teams for MongoDB
+	return nil
 }
